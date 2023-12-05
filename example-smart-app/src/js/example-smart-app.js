08290a6e7e3f8f2054b1ab31058e3c7e14ae56d3
Nicole Wojcik <nwojcik@silvercross.org>
2023-12-05 10:50:15 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,14 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: [
+						'http://loinc.org|8302-2'		// body height
+						//, 'http://loinc.org|8462-4'		// Diastolic BP
+						//, 'http://loinc.org|8480-6'		// Systolic BP
+						, 'http://loinc.org|2085-9'		// Cholesterol in HDL
+						, 'http://loinc.org|2089-1'		// Cholesterol in LDL
+						, 'http://loinc.org|85354-9'	// BP Systolic and Diastolic, changed from 55284-4
+						]
                       }
                     }
                   });
@@ -37,8 +42,8 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('85354-9'),'8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
